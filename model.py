197194f9eaa36596a18350246d699884aaa7bf81
Brandon M. Burroughs <brandonmburroughs@gmail.com>
2017-01-29 16:28:09 -0500

    Refactored model code and added convolutions

--- model.py
+++ model.py
@@ -70,7 +70,7 @@ def reformat(dataset, labels):
     ndarray, ndarray
         The reformatted images and corresponding labels arrays
     """
-    dataset = dataset.reshape((-1, image_width * image_height)).astype(np.float32)
+    dataset = dataset.reshape((-1, image_height, image_width, 1)).astype(np.float32)
     label_names = np.array([
         'Absolute_ABS_HAT0X_-1.png',
         'Absolute_ABS_HAT0X_0.png',
@@ -101,6 +101,155 @@ def accuracy(predictions, labels):
         / predictions.shape[0])
 
 
+def convolution_layer_variable_constructor(kernel_list):
+    """Build convolution layer variables given a list kernels.
+
+    Parameters
+    ----------
+    kernel_list : list
+        A list of number of nodes in each layer
+
+    Returns
+    -------
+    list (weights)
+        The list of weight variables
+
+    list (biases)
+        The list of bias variables
+    """
+    weights = []
+    biases = []
+    for kernel_num in range(len(kernel_list)):
+        weights.append(
+            tf.Variable(
+                tf.truncated_normal(
+                    kernel_list[kernel_num]
+                ), name="conv_weights_{}".format(kernel_num)
+            )
+        )
+
+        biases.append(
+            tf.Variable(
+                tf.zeros(
+                    [kernel_list[kernel_num][-1]]
+                ), name="conv_biases_{}".format(kernel_num)
+            )
+        )
+
+    return weights, biases
+
+
+def convolution_layer_constructor(dataset, weights_list, biases_list, strides_list):
+    """Given an original dataset, a list of weights, and a list of biases, construct the convolution layers.
+
+    Parameters
+    ----------
+    dataset : tf dataset
+        The original dataset
+
+    weights_list : list of tf kernel weights Variables
+        A list of kernel weights variables
+
+    biases_list : list of tf bias Variables
+        A list of bias variables
+
+    strides_list : list
+        A list of strides for each step
+
+    Returns
+    -------
+    logit
+        The logits for the output layer
+    """
+    conv_layers = [dataset]
+    for i in range(len(weights_list)):
+        conv_layers.append(
+                tf.nn.relu(
+                    tf.nn.conv2d(conv_layers[-1],
+                                 weights_list[i],
+                                 strides=[1, strides_list[i], strides_list[i], 1],
+                                 padding="VALID"
+                                 ) + biases_list[i]
+                )
+        )
+
+    return conv_layers[-1]
+
+
+def fully_connected_layer_variable_constructor(layer_list):
+    """Build fully connected layer variables given a list of number of nodes in each layer.  The first layer should be the
+    number of inputs while the last layer will be the number of outputs.
+
+    Parameters
+    ----------
+    layer_list : list
+        A list of number of nodes in each layer
+
+    Returns
+    -------
+    list (weights)
+        The list of weight variables
+
+    list (biases)
+        The list of bias variables
+    """
+    weights = []
+    biases = []
+    for layer_num in range(1, len(layer_list)):
+        weights.append(
+            tf.Variable(
+                tf.truncated_normal(
+                    [layer_list[layer_num - 1], layer_list[layer_num]]
+                ), name="weights_{}".format(layer_num)
+            )
+        )
+
+        biases.append(
+            tf.Variable(
+                tf.zeros(
+                    [layer_list[layer_num]]
+                ), name="biases_{}".format(layer_num)
+            )
+        )
+
+    return weights, biases
+
+
+def fully_connected_layer_constructor(dataset, weights_list, biases_list, keep_prob=1.0):
+    """Given an original dataset, a list of weights, and a list of biases, construct the fully connected layers.
+
+    Parameters
+    ----------
+    dataset : tf dataset
+        The original dataset
+
+    weights_list : list of tf weights Variables
+        A list of weights variables
+
+    biases_list : list of tf bias Variables
+        A list of bias variables
+
+    keep_prob : float
+        The probability of keeping a layer (as opposed to dropping it out)
+
+    Returns
+    -------
+    logit
+        The logits for the output layer
+    """
+    layers = [dataset]
+    for i in range(len(weights_list) - 1):
+        layers.append(
+            tf.nn.dropout(
+                tf.nn.relu(
+                    tf.matmul(layers[-1], weights_list[i]) + biases_list[i]
+                ), keep_prob
+            )
+        )
+
+    return tf.matmul(layers[-1], weights_list[-1]) + biases_list[-1]
+
+
 # Load images
 images, labels = load_images("data")
 num_images = images.shape[0]
@@ -116,40 +265,53 @@ test_dataset, test_labels = reformat(images[test_idx, :, :], labels[test_idx])
 
 # Tensorflow graph
 batch_size = 128
-num_hidden_nodes = 1024
 
 graph = tf.Graph()
 with graph.as_default():
-    # Input data. For the training data, we use a placeholder that will be fed
-    # at run time with a training minibatch.
-    tf_train_dataset = tf.placeholder(tf.float32,
-                                      shape=(batch_size, image_width * image_height))
+    # Input data
+    tf_train_dataset = tf.placeholder(tf.float32, shape=(batch_size, image_height, image_width, 1))
     tf_train_labels = tf.placeholder(tf.float32, shape=(batch_size, num_labels))
     tf_valid_dataset = tf.constant(valid_dataset)
     tf_test_dataset = tf.constant(test_dataset)
 
-    # Variables.
-    weights1 = tf.Variable(tf.truncated_normal([image_width * image_height, num_hidden_nodes]))
-    biases1 = tf.Variable(tf.zeros([num_hidden_nodes]))
-    weights2 = tf.Variable(tf.truncated_normal([num_hidden_nodes, num_labels]))
-    biases2 = tf.Variable(tf.zeros([num_labels]))
+    # Convolution kernels
+    conv_kernel_list = [
+        [5, 5, 1, 24],
+        [5, 5, 24, 36],
+        [5, 5, 36, 48],
+        [3, 3, 48, 64],
+        [3, 3, 64, 64]
+    ]
+    conv_stride_list = [2, 2, 2, 1, 1]
+    conv_weights, conv_biases = convolution_layer_variable_constructor(conv_kernel_list)
 
-    # Training computation.
-    hidden = tf.nn.relu(tf.matmul(tf_train_dataset, weights1) + biases1)
-    logits = tf.matmul(hidden, weights2) + biases2
+    convs_output = convolution_layer_constructor(tf_train_dataset, conv_weights, conv_biases, conv_stride_list)
+    convs_output_elements = convs_output.get_shape().num_elements() / batch_size
+
+    # Fully connected layer weights and biases
+    fcl_layer_list = [convs_output_elements, 1024, 256, 64, 16, num_labels]
+    fcl_weights, fcl_biases = fully_connected_layer_variable_constructor(fcl_layer_list)
+
+    # Training computation
+    logits = fully_connected_layer_constructor(tf.reshape(convs_output, shape=(batch_size, convs_output_elements)),
+                                               fcl_weights, fcl_biases)
     loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits, tf_train_labels))
 
-    # Optimizer.
+    # Optimizer
     optimizer = tf.train.GradientDescentOptimizer(0.5).minimize(loss)
 
-    # Predictions for the training, validation, and test data.
+    # Predictions for the training, validation, and test data
     train_prediction = tf.nn.softmax(logits)
-    valid_hidden = tf.nn.relu(tf.matmul(tf_valid_dataset, weights1) + biases1)
-    valid_prediction = tf.nn.softmax(
-        tf.matmul(valid_hidden, weights2) + biases2)
-    test_hidden = tf.nn.relu(tf.matmul(tf_test_dataset, weights1) + biases1)
-    test_prediction = tf.nn.softmax(
-        tf.matmul(test_hidden, weights2) + biases2)
+    valid_convs_output = convolution_layer_constructor(tf_valid_dataset, conv_weights, conv_biases, conv_stride_list)
+    valid_logits = fully_connected_layer_constructor(tf.reshape(valid_convs_output,
+                                                                shape=(valid_idx.shape[0], convs_output_elements)),
+                                                     fcl_weights, fcl_biases)
+    valid_prediction = tf.nn.softmax(valid_logits)
+    test_convs_output = convolution_layer_constructor(tf_test_dataset, conv_weights, conv_biases, conv_stride_list)
+    test_logits = fully_connected_layer_constructor(tf.reshape(test_convs_output,
+                                                               shape=(test_idx.shape[0], convs_output_elements)),
+                                                    fcl_weights, fcl_biases)
+    test_prediction = tf.nn.softmax(test_logits)
 
 num_steps = 3001
 
